3e76347260386487910c2dd716c10280bd95eeda
Russellmiller <russellmiller49@gmail.com>
2025-10-02 08:04:17 -0700

    Initial Commit

---
+++ EndoReels/EndoReelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndoReelsApp.swift
+//  EndoReels
+//
+//  Created by Russell Miller on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct EndoReelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
